a276233e0332494f51f2331111c49f0dcd4b80dc
Jimmynijs <jimmynijs@gmail.com>
2020-04-29 12:04:44 +0200

    Shuriken schieten!
    Je kan nu Shuriken schieten met de e knop.

---
+++ Ninja Runner/Assets/ShurikenMovement.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class ShurikenMovement : MonoBehaviour
+{
+    public Rigidbody ShurikenRB;
+    public float ShurikenSpeed = 10000f;
+
+    void FixedUpdate()
+    {
+        ShurikenRB.AddForce(0, 0, ShurikenSpeed * Time.deltaTime, ForceMode.VelocityChange);         //Kracht naar voren
+    }
+        
+}
